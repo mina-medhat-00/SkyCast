c2a0c5821934c9a53c06b84b782ca461f9cca0e4
mina-medhat-00 <mina.medhat.3141@gmail.com>
2025-06-16 23:49:30 +0300

    fix navbar issues with styling and theme consistency

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-navbar",
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: "./navbar.component.html",
   styles: ``,
 })
